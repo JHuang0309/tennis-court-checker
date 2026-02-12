c6d99c6f69c63962c7f3a5eeb408c34bdfbcd6ba
Jayden Huang <jhuang.study@gmail.com>
2026-02-12 18:44:49 +1100

    Attempt to deploy 2 with fixed redundant axios import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react'
-import axios from "axios";
 import './App.css'
 import VenueCard from './components/VenueCard'
 import { VenueFilter } from './components/VenueFilter'
